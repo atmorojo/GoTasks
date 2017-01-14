66b86bb84e13f0cede88e939200c460dbc74af9f
Risyad Rais <satmo54@gmail.com>
2017-01-14 11:51:15 +0700

    Firts Commit

---
+++ 03_struct/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Task struct {
+	Todo      string
+	CreatedAt time.Time
+}
+
+func (t *Task) NewTask(newTask string) {
+	t.Todo = newTask
+	t.CreatedAt = time.Now()
+}
+
+func (t Task) TaskCreated() string {
+	return t.CreatedAt.Format(time.RFC822)
+}
+
+func main() {
+	var task_01 Task
+	task_01.NewTask("Kerjakan tugas Go")
+
+	fmt.Printf("Task %q dibuat pada %q\n", task_01.Todo, task_01.TaskCreated())
+}
